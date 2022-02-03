194940ac8ec3f439c3000c4d866d1d29a4202b78
anushareddy5 <anushareddydental@gmail.com>
2022-02-04 01:05:42 +0530

    add patch and delete endpoints

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
 
 //Getting one
 router.get('/:id', getSubscriber, (req, res) => {
-    res.send(res.subscriber.name)
+    res.json(res.subscriber)
 })
 
 //Creating one 
@@ -45,12 +45,43 @@ router.post('/', async (req, res) => {
     }
 })
 //Updating one
-router.patch('/:id', getSubscriber, (req, res) => {
-
+router.patch('/:id', getSubscriber, async (req, res) => {
+if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+}
+if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = res.body.subscribedToChannel
+}
+try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+} catch (err) {
+    res.status(400).json({ message: err.message })
+}
 })
-//Deleting one
-router.delete('/:id', getSubscriber, (req, res) => {
-
+//Deleting by ID 
+router.delete('/:id', getSubscriber, async (req, res) => {
+try{
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted Subscriber' }) 
+} catch (err) {
+    res.status(500).json({ message: err.message })
+}
 })
+
+
+//Deleting all
+router.delete('/', async (req, res) => {
+    try{
+        const subscribers = await Subscriber.find()
+        subscribers.forEach(async function(subscriber) {
+            subscriberDelete =  await Subscriber.findById(subscriber.id)
+            subscriberDelete.remove()
+        })
+        res.json({ message: 'Deleted Subscribers', "subscribers": subscribers }) 
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+    })
 
 module.exports = router
